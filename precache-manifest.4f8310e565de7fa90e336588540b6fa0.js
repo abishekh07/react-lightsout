1d2a573d868831fa4ebf6421fc2bde8c2cb0097d
Abishek H <habishek07@gmail.com>
2019-12-30 13:52:07 +0530

    initial commit

---
+++ precache-manifest.4f8310e565de7fa90e336588540b6fa0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5d01601d3e5ceeb35a3",
+    "url": "/static/css/main.ea3b8fd5.chunk.css"
+  },
+  {
+    "revision": "d5d01601d3e5ceeb35a3",
+    "url": "/static/js/main.d5d01601.chunk.js"
+  },
+  {
+    "revision": "776909c087cde9b4cdc6",
+    "url": "/static/js/1.776909c0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ab99fd8d5eb299749f15ffa4dcf5b6a3",
+    "url": "/index.html"
+  }
+];
